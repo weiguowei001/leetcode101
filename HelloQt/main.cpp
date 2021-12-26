ee4c39c3647f9c1c3c144dad15e05080e8b70506
weigw <15851830359@163.com>
2021-12-26 16:18:22 +0800

    modified:   main.cpp

--- HelloQt/main.cpp
+++ HelloQt/main.cpp
@@ -6,13 +6,6 @@
 int
 main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		// report version
-		std::cout << argv[0] << " Version " << HelloWorld_VERSION_MAJOR << "."
-				  << HelloWorld_VERSION_MINOR << std::endl;
-		std::cout << "Usage: " << argv[0] << " number" << std::endl;
-		return 1;
-	}
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
