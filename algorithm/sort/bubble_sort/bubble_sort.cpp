5795536afbdbe027e6ba9373f48f9eb02ae53bd5
weigw <weiguowei@zqykj.com>
2022-01-08 17:24:22 +0800

    bubble_sort

--- algorithm/sort/bubble_sort/bubble_sort.cpp
+++ algorithm/sort/bubble_sort/bubble_sort.cpp
@@ -1,6 +1,21 @@
 #include "bubble_sort.hpp"
+#include <utility>
 
 void
 bubble_sort(std::vector<int> &nums, int n)
 {
+	using namespace std;
+	bool swapped;
+	for (int i = 1; i < n; ++i) {
+		swapped = false;
+		for (int j = 1; j < n - i + 1; ++j) {
+			if (nums[j] < nums[j - 1]) {
+				swap(nums[j], nums[j - 1]);
+				swapped = true;
+			}
+		}
+		if (!swapped) {
+			break;
+		}
+	}
 }
