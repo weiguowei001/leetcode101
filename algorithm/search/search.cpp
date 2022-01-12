2f9ecfaa6ce4fd1898c4e3f24b68e626fd4b71fa
weigw <15851830359@163.com>
2022-01-12 20:20:52 +0800

    stack::maxAreaOfIsLand

--- algorithm/search/search.cpp
+++ algorithm/search/search.cpp
@@ -26,33 +26,34 @@ int
 maxAreaOfIsLand(std::vector<std::vector<int> > &grid)
 {
 	size_t m = grid.size(), n = m ? grid[0].size() : 0;
-	int local_area, area = 0, x, y;
+	int local_area, area = 0;
 	for (int i = 0; i < m; ++i) {
 		for (int j = 0; j < n; ++j) {
-			if (grid[i][j]) {
-				local_area = 1;
-				grid[i][j] = 0;
-				std::stack<std::pair<int, int> > island;
-				island.push({i, j});
-				while (!island.empty()) {
-					auto [r, c] = island.top();
-					island.pop();
-					for (int k = 0; k < 4; ++k) {
-						x = r + direction[k], y = c + direction[k + 1];
-						if (x >= 0 && x < m && y >= 0 && y < n &&
-							grid[x][y] == 1) {
-							grid[x][y] = 0;
-							++local_area;
-							island.push({x, y});
-						}
+			if (!grid[i][j]) {
+				continue;
+			}
+			local_area = 1;
+			grid[i][j] = 0;
+			std::stack<std::pair<int, int> > island;
+			island.push({i, j});
+			while (!island.empty()) {
+				auto [r, c] = island.top();
+				island.pop();
+				for (int k = 0; k < 4; ++k) {
+					int x = r + direction[k], y = c + direction[k + 1];
+					if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1) {
+						grid[x][y] = 0;
+						++local_area;
+						island.push({x, y});
 					}
 				}
-				area = std::max(area, local_area);
 			}
+			area = std::max(area, local_area);
 		}
 	}
 	return static_cast<int>(area);
 }
+
 } // namespace stack
 
 namespace recursive {
