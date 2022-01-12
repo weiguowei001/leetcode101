d0df1f67482fbfa71809a586ccbfe1244410cc6c
weigw <15851830359@163.com>
2022-01-12 23:36:19 +0800

    modified: tests

---
+++ algorithm/tests/testsearch.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "search.hpp"
+
+using namespace algorithm::search;
+
+int
+main()
+{
+	std::vector<std::vector<int> > friends_dfs_recursive{
+		{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+	std::cout << "dfs::recursive::findCircleNum: "
+			  << dfs::recursive::findCircleNum(friends_dfs_recursive) << "\n";
+
+	return 0;
+}
