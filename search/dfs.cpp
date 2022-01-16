13b5b0880798a6cbdeb538306a10b467fa064380
weigw <15851830359@163.com>
2022-01-16 21:13:07 +0800

    pacificAtlantic

--- search/dfs.cpp
+++ search/dfs.cpp
@@ -137,6 +137,55 @@ findCircleNum(std::vector<std::vector<int> > &friends)
 	}
 	return static_cast<int>(count);
 }
+
+
+void
+dfs_4(const std::vector<std::vector<int> > &matrix,
+	std::vector<std::vector<bool> > &can_reach, int r, int c)
+{
+	if (can_reach[r][c]) {
+		return;
+	}
+	can_reach[r][c] = true;
+	int x, y;
+	for (int i = 0; i < 4; ++i) {
+		x = r + direction[i], y = c + direction[i + 1];
+		if (x >= 0 && x < matrix.size() && y >= 0 && y < matrix[0].size() &&
+			matrix[r][c] <= matrix[x][y]) {
+			dfs_4(matrix, can_reach, x, y);
+		}
+	}
+}
+
+std::vector<std::vector<int> >
+pacificAtlantic(std::vector<std::vector<int> > &matrix)
+{
+	if (matrix.empty() || matrix[0].empty()) {
+		return {};
+	}
+
+	std::vector<std::vector<int> > ans;
+	int m = matrix.size(), n = m ? matrix[0].size() : 0;
+	std::vector<std::vector<bool> > can_reach_p(m, std::vector<bool>(n, false));
+	std::vector<std::vector<bool> > can_reach_a(m, std::vector<bool>(n, false));
+	for (int i = 0; i < m; ++i) {
+		dfs_4(matrix, can_reach_p, i, 0);
+		dfs_4(matrix, can_reach_a, i, n - 1);
+	}
+	for (int i = 0; i < n; ++i) {
+		dfs_4(matrix, can_reach_p, 0, i);
+		dfs_4(matrix, can_reach_a, m - 1, i);
+	}
+	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < n; ++j) {
+			if (can_reach_p[i][j] && can_reach_a[i][j]) {
+				ans.push_back(std::vector<int>{i, j});
+			}
+		}
+	}
+	return ans;
+}
+
 } // namespace recursive
 } // namespace dfs
 } // namespace search
--- search/dfs.hpp
+++ search/dfs.hpp
@@ -13,15 +13,14 @@ int maxAreaOfIsLand(std::vector<std::vector<int> > &grid);
 
 namespace recursive {
 
-int dfs_1(std::vector<std::vector<int> > &grid, int r, int c);
 int maxAreaOfIsLand_1(std::vector<std::vector<int> > &grid);
 
-int dfs_2(std::vector<std::vector<int> > &grid, int r, int c);
 int maxAreaOfIsLand_2(std::vector<std::vector<int> > &grid);
 
-void dfs_3(
-	std::vector<std::vector<int> > &friends, int i, std::vector<bool> &visited);
 int findCircleNum(std::vector<std::vector<int> > &friends);
+
+std::vector<std::vector<int> > pacificAtlantic(
+	std::vector<std::vector<int> > &matrix);
 
 } // namespace recursive
 } // namespace dfs
