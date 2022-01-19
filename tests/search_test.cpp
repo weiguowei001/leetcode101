bfe0a99b3e256233b567e01b2ab4c98c34d74dca
weigw <15851830359@163.com>
2022-01-19 22:23:45 +0800

    add test demo

--- tests/search_test.cpp
+++ tests/search_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "../search/dfs.hpp"
+#include "../search/backtracking.hpp"
 
 using namespace std;
 using namespace algorithm::search;
@@ -13,5 +14,12 @@ main()
 	cout << "dfs::recursive::findCircleNum: "
 			  << dfs::recursive::findCircleNum(friends_dfs_recursive) << "\n";
 
+	vector<vector<int> > combine = backtracking::combine(4, 2);
+	cout << "backtracking::combine:\n";
+	for (auto &&v : combine)
+	{
+		cout << v[0] << v[1] << "\n";
+	}
+	
 	return 0;
 }
